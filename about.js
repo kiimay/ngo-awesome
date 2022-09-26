78bb27ee08617362b115594eb93d4fcf672e21a7
kiimay <84385320+kiimay@users.noreply.github.com>
2022-09-26 18:21:51 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.storys-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('change')
+        btn.nextElementSibling.classList.toggle('change')
+    })
+})
